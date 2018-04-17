82a671fc93af3ac48d0128bca55b767a63039b35
Jeffrey Ricker <jeffrey.ricker@gmail.com>
2018-04-17 06:53:55 -0500

    added User class, added foreign key to Blog class relating the two classes

--- main.py
+++ main.py
@@ -19,10 +19,23 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
+    owner_id = db.Column(db.Integer,  db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 def get_blog_entries():
     return Blog.query.all()
@@ -41,7 +54,7 @@ def add_blog():
             flash("Don't leave your title or body blank!", 'error')
             return render_template('newpost.html', blog_title=blog_title, body=body)
 
-        new_blog = Blog(title=blog_title, body=body)
+        new_blog = Blog(title=blog_title, body=body, owner)
         db.session.add(new_blog)
         db.session.commit()
         #redirect user to page displaying blog post they just created
